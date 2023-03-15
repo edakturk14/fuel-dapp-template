ff769e89e53cc1c3d36399b864911310fa1e56e3
Carlos Sánchez <oceanrdn@gmail.com>
2023-03-15 19:07:02 +0100

    Header & Footer. New Fuel color / logo. Tweaks to the contract display

---
+++ packages/nextjs/components/PageFooter.tsx
@@ -0,0 +1,25 @@
+export const PageFooter = () => {
+  return (
+    <div className="text-center my-5">
+      Built by{" "}
+      <a
+        href="https://github.com/carletex"
+        className="link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Carlos
+      </a>{" "}
+      and{" "}
+      <a
+        href="https://github.com/edakturk14"
+        className="link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Eda
+      </a>{" "}
+      with ❤️
+    </div>
+  );
+};
---
+++ packages/nextjs/components/PageHeader.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import { GithubIcon } from "./icons/GithubIcon";
+
+export const PageHeader = () => {
+  return (
+    <div className="flex gap-8 py-4 items-center">
+      <p className="text-center text-2xl text-primary">
+        <Image
+          className="inline-block pr-2"
+          src="/logo.png"
+          alt="Fuel Logo"
+          width={34}
+          height={40}
+        />
+        Fuel App
+      </p>
+      <ul className="flex gap-6 ">
+        <li>
+          <a
+            href="https://fuellabs.github.io/block-explorer-v2/"
+            target="_blank"
+            rel="noreferrer"
+            className="link-hover"
+          >
+            Fuel Block Explorer
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://forum.fuel.network/"
+            target="_blank"
+            rel="noreferrer"
+            className="link-hover"
+          >
+            Fuel Forum
+          </a>
+        </li>
+        <li className="flex gap-1">
+          <GithubIcon />
+          <a
+            href="https://github.com/edakturk14/fuel-dapp-template/"
+            target="_blank"
+            rel="noreferrer"
+            className="link-hover"
+          >
+            Fork me
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
