3c5e7fd612ffe794e0dabf10a8ac47a27b054748
Eda Akturk <edaakturk@Edas-MacBook-Air.local>
2023-03-16 12:29:31 -0500

    Connect to browser wallet

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import { ContractAbi, ContractAbi__factory } from "../contracts";
-import { BN, Wallet } from "fuels";
+import { ContractAbi,ContractAbi__factory } from "../contracts";
+import { BN } from "fuels";
 import { PageHeader } from "../components/PageHeader";
 import { PageFooter } from "../components/PageFooter";
 import { ResourcesLinks } from "../components/ResourcesLinks";
 
+/*
 interface NetworkConfig {
   [key: string]: {
     provider: string;
@@ -18,47 +19,80 @@ const AVAILABLE_NETWORKS: NetworkConfig = {
     provider: "https://node-beta-2.fuel.network/graphql",
   },
 };
-
 // Selected network
 const selectedNetwork: keyof NetworkConfig =
   process.env.NEXT_PUBLIC_NETWORK ?? "localhost";
 const network = AVAILABLE_NETWORKS[selectedNetwork];
 
-const WALLET_SECRET = process.env.NEXT_PUBLIC_WALLET_SECRET ?? "";
-// Handle error.
-const CONTRACT_ID = process.env.NEXT_PUBLIC_CONTRACT_ID ?? "";
+//const WALLET_SECRET = process.env.NEXT_PUBLIC_WALLET_SECRET ?? "";
+*/
 
-let contract: ContractAbi;
-try {
-  const wallet = Wallet.fromPrivateKey(WALLET_SECRET, network.provider);
-  contract = ContractAbi__factory.connect(CONTRACT_ID, wallet);
-} catch (e) {
-  console.log("Error", e);
+declare global {
+  interface Window {
+    fuel?: any;
+  }
 }
+
+const CONTRACT_ID = process.env.NEXT_PUBLIC_CONTRACT_ID ?? "";
 
 export default function Home() {
   const [counter, setCounter] = useState(0);
+  const [connected, setConnected] = useState<boolean>(false);
+  const [account, setAccount] = useState<string>("");
   const [isLoadingTx, setIsLoadingTx] = useState(false);
   const [errorMessage, setErroMessage] = useState("");
 
   useEffect(() => {
-    const getCounterValue = async () => {
-      const { value } = await contract.functions.counter().get();
-      setCounter(Number(value));
-    };
+    setTimeout(() => {
+      checkConnection();
+      setIsLoadingTx(true);
+    }, 200);
+    if (connected) getCounter();
+  }, [connected]);
 
-    if (!contract) return;
-    getCounterValue();
-  }, []);
+  async function connect() {
+    if (window.fuel) {
+      try {
+        await window.fuel.connect();
+        const accounts = await window.fuel.accounts();
+        setAccount(accounts[0]);
+        setConnected(true);
+      } catch (err) {
+        console.log("error connecting: ", err);
+      }
+    }
+  }
+  async function disconnect() {
+    if (window.fuel && window.fuel.isConnected) {
+      await window.fuel.disconnect();
+      setAccount("");
+      setConnected(false);
+    }
+  }
+
+  async function checkConnection() {
+    if (window.fuel && window.fuel.isConnected) {
+      const isConnected = await window.fuel.isConnected();
+      if (isConnected) {
+        const accounts = await window.fuel.accounts();
+        setAccount(accounts[0]);
+        setConnected(true);
+      }
+    }
+  }
+  async function getCounter() {
+    const wallet = await window.fuel.getWallet(account);
+    const contract = ContractAbi__factory.connect(CONTRACT_ID, wallet);
+    const { value } = await contract.functions.counter().get();
+    setCounter(Number(new BN(value)));
+  }
 
   const incrementCounter = async () => {
-    setIsLoadingTx(true);
-    let data: { value: BN };
+    const wallet = await window.fuel.getWallet(account);
+    const contract = ContractAbi__factory.connect(CONTRACT_ID, wallet);
+    //let data: { value: BN };
     try {
-      data = await contract.functions
-        .increment()
-        .txParams({ gasPrice: 1 })
-        .call();
+      await contract.functions.increment().txParams({ gasPrice: 1 }).call();
     } catch (e) {
       console.error("~~ increment counter tx error", e);
       setIsLoadingTx(false);
@@ -67,7 +101,7 @@ export default function Home() {
       setErroMessage(errorMsg);
       return;
     }
-    setCounter(Number(data.value));
+    //setCounter(Number(data.value));
     setIsLoadingTx(false);
     setErroMessage("");
   };
@@ -76,36 +110,33 @@ export default function Home() {
     <div className="px-8 flex flex-col min-h-screen">
       <PageHeader />
       <main className="flex items-center flex-col py-16 flex-grow">
-        {!contract && (
-          <div className="alert alert-error max-w-sm mb-4">
-            <div>
-              <span>
-                Contract not connected. Make sure you have defined the right
-                values on the <code>.env</code> file.
-              </span>
-            </div>
-          </div>
-        )}
+        <div className="flex items-center flex-col">
+          {connected ? (
+            <>
+              <h2 className="text-2xl font-bold text-primary mb-2">
+                Interact with your Fuel Contract
+              </h2>
+              <p>Current counter value</p>
+              <p className="text-4xl font-bold">{counter}</p>
+              <button
+                className={`mt-6 btn btn-primary ${
+                  isLoadingTx ? "loading" : ""
+                }`}
+                onClick={incrementCounter}
+              >
+                Increment Counter
+              </button>
+              <button className="mt-6 btn btn-primary" onClick={disconnect}>
+                Disconnect
+              </button>
+            </>
+          ) : (
+            <button className="mt-6 btn btn-primary" onClick={connect}>
+              Connect
+            </button>
+          )}
+        </div>
 
-        <h2 className="text-2xl font-bold text-primary mb-2">
-          Interact with your Fuel Contract
-        </h2>
-        <p>Current counter value</p>
-        <p className="text-4xl font-bold">{counter}</p>
-        <button
-          className={`mt-6 btn btn-primary ${isLoadingTx ? "loading" : ""}`}
-          onClick={incrementCounter}
-        >
-          Increment Counter
-        </button>
-
-        {errorMessage && (
-          <div className="alert alert-error max-w-sm mt-4">
-            <div>
-              <span>{errorMessage}</span>
-            </div>
-          </div>
-        )}
         <ResourcesLinks />
       </main>
       <PageFooter />
